704dee106838fd4cddff39c16481bd410efe3561
deltaplanoo <pietro.ciani01@gmail.com>
2025-02-23 20:34:23 +0100

    linked: implementation done

--- disjoint_linked.py
+++ disjoint_linked.py
@@ -20,23 +20,34 @@ class NodeList:
     def __init__(self, node):
         self.head = node
         self.tail = node
-        self.nodes = []
+        self.nodes = []  # Initialize with the single node
         self.size = 1
-    
-    def union(self, list):              # UNION() attach nodeList to the end of the current list
-        self.size += list.size
-        list.set_head(self.tail)
-        self.tail.set_next(list.head)   # self tail -> list head
-        self.tail = list.tail
-        for node in list.nodes:
-            node.set_list(self)
 
     def set_head(self, head):
         self.head = head
-    
+
     def set_tail(self, tail):
         self.tail = tail
 
+    def union(self, other_list):
+        """
+        Union the current list (self) with another list (other_list).
+        Attach all members of other_list at the end of the current list.
+        """
+        if self == other_list:
+            return
+        self.tail.next = other_list.head
+        self.tail = other_list.tail
+        self.nodes.extend(other_list.nodes)
+        self.size += other_list.size
+        for node in other_list.nodes:
+            node.list = self
+
+    def print(self):
+        """Print all elements in the set"""
+        for node in self.nodes:
+            print(node.key, end=" ")
+        print()
 
 
 class DisjointSetHandler:
@@ -55,15 +66,20 @@ class DisjointSetHandler:
     def find_set(self, node):
         """Find the set containing the given node"""
         return node.find_list()
-    
-    def union(self, list1, list2):
-        """Union the sets"""
-        if (list1 != list2):
-            self.sets.remove(list2)     # Remove the second list from our sets since it's being merged
-            list1.union(list2)          # Perform the union operation
-    
-    def print_sets(self):
-        """Print all sets in a readable format"""
-        sets = self.get_all_sets()
-        for i, set_elements in enumerate(sets):
-            print(f"Set {i + 1}: {set_elements}")
+
+    def union(self, i, j):
+        """Union the sets at indexes i and j."""
+        if i == j:
+            raise ValueError("Cannot union the same set with itself.")
+        list1 = self.sets[i]
+        list2 = self.sets[j]
+
+        list1.union(list2)
+        self.sets.pop(j)
+
+    def print(self):
+        """Print all sets with their elements"""
+        for i in range(len(self.sets)):
+            print(f"Set {i}: ")
+            self.sets[i].print()
+        print("\n")
--- main.py
+++ main.py
@@ -1,8 +1,21 @@
+import random
 import disjoint_linked as dl
 
-randoms = [random.randint(0, 999) for _ in range(33)]
+n = 10
+randoms = [random.randint(0, 999) for _ in range(n)]
+
+# Create a SINGLE instance of DisjointSetHandler
+handler = dl.DisjointSetHandler()
 
 # Create a disjoint set for each node
 for i in randoms:
-	dl.DisjointSetHandler().make_set(i)
+    handler.make_set(i)
 
+# Union every set to the first set
+for i in range(0, n-1):
+	handler.print()
+	j = random.randrange(1, n-i)
+	handler.union(0, j)
+
+# Print all sets
+handler.print()
