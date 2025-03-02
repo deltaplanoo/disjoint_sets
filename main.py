7b93b76ef2e7328b2ac324923fdf44986916b325
deltaplanoo <pietro.ciani01@gmail.com>
2025-03-02 15:14:31 +0100

    main: print db content

--- main.py
+++ main.py
@@ -1,25 +1,9 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import random
-import disjoint_linked as dl
-import graph as g
 import time
-import dao
-"""
-### GENERAL ###
-n = 500
-e = 499
-handler = dl.DisjointSetHandler()
-nodes = [dl.Node(i) for i in range(n)]  # Create n Nodes with keys 0 to n-1
+from DAO import dao
 
-### GRAPH GENERATION ###
-graph = g.Graph(nodes)
-graph.generate_edges(e)
-graph.print_graph()
-
-### CONNECTED COMPONENTS ###
-start = time.time()
-handler.find_connected_components(graph)
-end = time.time()
-handler.print()
-print(f"Time taken: {end - start:.6f} seconds")
-"""
 dao.display_results()
