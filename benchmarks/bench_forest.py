90f8b79881e3fd9c65f9c7e6fd329a51628bfcab
deltaplanoo <pietro.ciani01@gmail.com>
2025-03-03 21:22:57 +0100

    benchmark: forest

--- benchmarks/bench_forest.py
+++ benchmarks/bench_forest.py
@@ -7,31 +7,26 @@ from structures import disjoint_forest as d
 from structures import graph as g
 from DAO import dao
 
-def bench(n, density, m_max, p):
+def bench(n_list, density):
 	print("Benching forest")
 	dao.create_database()
-	e = round(density * (n*(n-1))/2)
-	m = 0
-	while (m <= m_max):
+	for n in n_list:
 		### GENERAL ###
 		handler = d.DisjointSetHandler()
 		nodes = [d.Node(i) for i in range(n)]
 		### GRAPH GENERATION ###
+		e = round(density * (n*(n-1))/2)
 		graph = g.Graph(nodes)
 		graph.generate_edges(e)
-		print(f"Graph with {n} nodes and {e} edges -> density: {(2*e)/(n*(n-1)):.6f}")
+		print(f"Graph with {n} nodes and {e} edges -> computed density: {(2*e)/(n*(n-1)):.6f}")
 
 		### FIND CONNECTED COMPONENTS ###
-		start = time.time()
+		start = time.perf_counter_ns()
 		m = handler.find_connected_components(graph)
-		end = time.time()
-		exec_time = end - start
-		print(f"m: {m} - time: {exec_time:.6f} seconds")
+		end = time.perf_counter_ns()
+		exec_time = (end - start) / 1e9
+
+		print(f"(Normal LL)   m: {m} - time: {exec_time:.6f} seconds")
 		print()
 
-		# Benchmark results
 		dao.insert_result("forest", n, m, exec_time)
-		
-		# Next benchmark
-		n = n * p
-		e = round(density * (n*(n-1))/2)
